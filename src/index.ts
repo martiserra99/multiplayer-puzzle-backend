da803d6b38825e43288a1bdda42f56e2f3c2232a
Marti Serra <martiserramolina99@gmail.com>
2024-01-18 12:08:24 +0100

    Created project

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log();
+});
